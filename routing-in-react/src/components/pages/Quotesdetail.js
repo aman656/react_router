7bbf28cb7c401f98f01368bbc1cd233bc0be65b1
Aman Siddiqui <amansiddiquias123@gmail.com>
2021-09-24 21:08:40 +0500

    Adding some nested routes

--- routing-in-react/src/components/pages/Quotesdetail.js
+++ routing-in-react/src/components/pages/Quotesdetail.js
@@ -1,7 +1,8 @@
-import { useParams } from "react-router"
+import { useParams,useLocation } from "react-router"
 import { Route } from "react-router"
 import Comments from '../comments/Comments'
 import HighlightedQuote from '../quotes/HighlightedQuote'
+import { Link } from "react-router-dom"
 
 const DUMMY_LIST = [{
     id:"q1",author:"No one",text:"Follow someone"},
@@ -10,6 +11,12 @@ const DUMMY_LIST = [{
 
 const QuoteDetail = ()=>{
     const params=  useParams()
+    const location = useLocation()
+    let isShow ;
+    console.log(location)
+    if(location.pathname==="/quotes/" + params.id){
+        isShow = true
+    }
     const quote = DUMMY_LIST.find((quote)=>  quote.id===params.id)
     let error;
 
@@ -20,10 +27,14 @@ const QuoteDetail = ()=>{
     return(
         <div>
         {quote ?<HighlightedQuote  author = {quote.author} text = {quote.text} />: <p>{error}</p>}
+        <div className="centered">
+            {isShow && <Link   className="btn--flat"         to={`/quotes/${params.id}/comments`}>Load Comments</Link>}
+        </div>
         <Route path={`/quotes/${params.id}/comments`}>
             <Comments/>
 
         </Route>
+        
 
         </div>
     )
